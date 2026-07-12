ab8d59384df648e361295bb116e343f82caf0e87
harshita kapoor <harshitakapoor1021@gmail.com>
2026-07-12 18:07:51 +0530

    Added root route

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Backend is running successfully ");
+});
+
 app.get("/profile",auth,(req,res)=>{
     res.json({message:"This is a protected route",user:req.user});
 });
