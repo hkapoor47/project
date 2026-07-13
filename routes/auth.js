84c7ee065cbb504a6115b353e23693d3ab41154e
harshita kapoor <harshitakapoor1021@gmail.com>
2026-07-13 11:19:32 +0530

    Added root route

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+ try{
     const { name, email, password } = req.body;
     if (!name) {
        return res.status(400).json({
@@ -32,6 +33,12 @@ router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hashedPassword });
     res.json({ message: 'User registered successfully', user });
+}catch (err) {
+        console.error(err);   // <-- VERY IMPORTANT
+        res.status(500).json({
+            message: "Internal Server Error",
+        });
+    }
 });
 
 router.post('/login', async (req, res) => {
