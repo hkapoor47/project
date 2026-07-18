0525da295ab772f396649d8f2b47e2a5994aface
harshita kapoor <harshitakapoor1021@gmail.com>
2026-07-18 19:24:10 +0530

    Added root route

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,7 @@ function auth(req,res,next){
         return res.status(401).json({message:"No token provided"});
     }
     try {
-        const decoded=jwt.verify(token,"secretkey");
+        const decoded=jwt.verify(token,process.env.JWT_SECRET);
         req.user=decoded;
         next();
     }catch(error){
