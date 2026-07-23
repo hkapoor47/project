611a4bfb593eb4a7c527f76dba949b214860060a
harshita kapoor <harshitakapoor1021@gmail.com>
2026-07-23 11:24:33 +0530

    Fix Gemini model

--- services/llmService.js
+++ services/llmService.js
@@ -6,7 +6,7 @@ const ai = new GoogleGenAI({
 
 async function askLLM(prompt) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash-lite",
+    model: "gemini-2.5-flash",
     contents: prompt,
   });
 
