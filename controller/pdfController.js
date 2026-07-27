daf39a991b3027e9f08427ddb81a770e38566eb2
harshita kapoor <harshitakapoor1021@gmail.com>
2026-07-27 15:28:58 +0530

    speaker wise transcript added

--- controller/pdfController.js
+++ controller/pdfController.js
@@ -6,26 +6,19 @@ const {
 async function handleGeneratePdf(req, res) {
 
     try {
-
         console.log("Generate PDF API hit");
-
         const { geminiResponse } = req.body;
-
         // Check if Gemini response exists
         if (!geminiResponse) {
             return res.status(400).json({
                 message: "Gemini response is required"
             });
         }
-
         console.log("Gemini response received:");
-        console.log(geminiResponse);
-
+        console.log(geminiResponse)
         // Generate PDF
         const pdf = await generatePdf(geminiResponse);
-
         console.log("PDF generated:", pdf.filePath);
-
         // Send PDF to frontend
         return res.download(
             pdf.filePath,
