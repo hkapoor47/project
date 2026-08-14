15137aeedc18dd249aa5413050e24f58a60833ff
harshita kapoor <harshitakapoor1021@gmail.com>
2026-08-14 17:18:18 +0530

    server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const { Server } = require("socket.io");
 const auth = require("./middleware/authMiddleware");
 
 const app = express();
-
-const PORT =
-  process.env.PORT || 5000;
+const PORT =process.env.PORT || 5000;
 
 const speech = require("./routes/speech");
 const agora = require("./routes/agora");
