cf455eb4f67885d82a5e95772c079d68418f6eea
harshita kapoor <harshitakapoor1021@gmail.com>
2026-08-21 15:43:43 +0530

    Fix project structure

--- routes/agora.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { RtcTokenBuilder, RtcRole } = require("agora-token");
-const { handleGetToken } = require("../controller/agoraController");
-
-router.get("/token", handleGetToken);
-
-module.exports = router;
--- routes/llm.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const { generateAnswer } = require("../controller/llmController");
-
-router.post("/summarize", generateAnswer);
-
-module.exports = router;
